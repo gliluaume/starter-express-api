6f87fa74009ffc66c5aa0c49f08652be284f7327
Guillaume Salicis <gsalicis@d-edge.com>
2023-02-07 15:15:00 +0100

    update to handle everything

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const port = process.env.PORT || 3000
+
+const log = (...args) => console.log(
+    (new Date()).toJSON(),
+    args.map(
+        (item) => typeof item === "object" ? JSON.stringify(item) : item).join(", "))
+
+const reqInfos = (req) => {
+    const { method, url, headers, query, body, path } = req;
+    return { method, url, headers, query, body, path }
+}
+
+app.all('*', (req, res) => {
+    const meta = reqInfos(req)
+    log(meta)
+    res.send(`you said: ${meta.method} ${meta.url}`)
 })
-app.listen(process.env.PORT || 3000)
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
